2cbc3999e372bfe2b9d8ddabbcff0ed09587ded8
khubrani2a-gif <khubrani2a@gmail.com>
2026-07-26 22:38:52 +0300

    Add lightweight daily planning tools

--- tests/index-sync-contract.test.js
+++ tests/index-sync-contract.test.js
@@ -82,7 +82,7 @@ test("mobile scroll containers clear the fixed navigation and iPhone safe area",
 });
 
 test("today overview and responsive quick navigation remain wired",()=>{
-  ["todayOverviewTitle","todayPrayerValue","todayTaskValue","todayQuranValue","todayWaterValue","todayFocus","weeklyGoalBanner","weeklyGoalText","weeklyGoalDone","tomorrowItems","quickTaskBtn","quickExpenseBtn","quickWaterBtn","quickTemplatesBtn","shareWeekBtn","quickTemplateUndoBtn","quickTemplateStatus","quickCustomTemplateBtn","quickManageTemplatesBtn","quickCustomTemplateList"].forEach(id=>{
+  ["todayOverviewTitle","todayPrayerValue","todayTaskValue","todayQuranValue","todayWaterValue","todayFocus","weeklyGoalBanner","weeklyGoalText","weeklyGoalDone","tomorrowItems","quickTaskBtn","quickExpenseBtn","quickWaterBtn","quickTemplatesBtn","shareWeekBtn"].forEach(id=>{
     assert.match(html,new RegExp('id="'+id+'"'),id);
   });
   ["today","iman","sport","expenses","more"].forEach(view=>assert.ok(html.includes('data-app-view="'+view+'"'),view));
@@ -91,18 +91,6 @@ test("today overview and responsive quick navigation remain wired",()=>{
   assert.match(html,/function renderTomorrowPanel\(/);
   assert.match(html,/function smartWeekSummary\(/);
   assert.match(html,/function smartInit\(/);
-  assert.match(html,/function smartUndoLastTemplate\(/);
-  assert.match(html,/function smartTemplateNotice\(/);
-  assert.match(html,/function smartOpenQuickTask\(/);
-  assert.match(html,/function smartOpenCustomTemplate\(/);
-  assert.match(html,/function smartManageCustomTemplates\(/);
-  assert.match(html,/function smartOpenTemplateEditor\(/);
-  assert.match(html,/function smartAskInTemplateManager\(/);
-  assert.match(html,/function smartSetTemplateHidden\(/);
-  assert.match(html,/data-smart-template-hide/);
-  assert.match(html,/SMART_CUSTOM_TEMPLATES_KEY/);
-  assert.match(html,/SMART_TEMPLATE_SETTINGS_KEY/);
-  assert.match(html,/quickTemplatesBtn"\)\.onclick=[\s\S]*?scrollIntoView/);
   assert.match(html,/renderTodayOverview\(\);\s*\n\s*updateStreak/);
 });
 
